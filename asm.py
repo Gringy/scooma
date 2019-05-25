334a18a27e93655b75ada46689be44f079338eef
Kanev <egor.kanev.97@mail.ru>
2019-05-25 17:52:52 +0300

    dict:find (v0.0.5)

--- asm.py
+++ asm.py
@@ -98,7 +98,7 @@ def writeFile(name):
 	outFile.write(bytearray(memory))
 
 def main():
-	print("asm for scooma v0.0.4")
+	print("asm for scooma v0.0.5")
 	if len(sys.argv) == 2:
 		importCodes()
 		readFile(sys.argv[1])
