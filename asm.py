4e1646da1421f79f860fb0cb9376aee79dbc598d
Kanev <egor.kanev.97@mail.ru>
2019-05-25 16:12:05 +0300

    headers for new words (v0.0.4)

--- asm.py
+++ asm.py
@@ -98,7 +98,7 @@ def writeFile(name):
 	outFile.write(bytearray(memory))
 
 def main():
-	print("asm for scooma v0.0.3")
+	print("asm for scooma v0.0.4")
 	if len(sys.argv) == 2:
 		importCodes()
 		readFile(sys.argv[1])
